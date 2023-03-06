318c13bb35a4893afe1733f978365cbcd0b4cfc9
Polina Korkina <pvkorkina@gmail.com>
2023-03-06 18:30:51 +0500

    added 4-module 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let row of table.rows) {
+    
+    switch (row.cells[3].dataset.available) {
+      case 'true':
+        row.classList.add('available');
+        break;
+      case 'false':
+        row.classList.add('unavailable');
+        break;
+      default:        
+        if (row.parentNode.tagName === 'TBODY') {
+          row.hidden = true;
+        }
+    }
+
+    switch (row.cells[2].textContent) {
+      case 'm':
+        row.classList.add('male');
+        break;
+      case 'f':
+        row.classList.add('female');
+        break;
+    }
+
+    if (row.cells[1].textContent < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+
+  }
+  
 }
