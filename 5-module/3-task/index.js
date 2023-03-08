4a7cec98700103b0fd23deb9a579d472982619d8
Polina Korkina <pvkorkina@gmail.com>
2023-03-09 02:41:17 +0500

    added 5-module 3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  const width = document.querySelector('[data-carousel-holder]').offsetWidth;
+  // const width = 500;
+  console.log(width);
+  const slidesCount = document.querySelectorAll('.carousel__slide').length;
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  let position = 0;
+
+  arrowLeft.style.display = 'none';
+
+  arrowLeft.onclick = function() {
+    arrowRight.style.display = '';
+    position += width;
+    translate(position);
+    if (position >= 0) this.style.display = 'none';
+  }
+  
+  arrowRight.onclick = function() {
+    arrowLeft.style.display = '';
+    position -= width;
+    translate(position);
+    if ( position <= -1 * width * (slidesCount - 1) ) this.style.display = 'none';
+  }
+
+  function translate(position) {
+    document.querySelector('.carousel__inner').style
+      .transform = `translateX(${position}px)`;
+  }
 }
