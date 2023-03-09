bb367ab6c9e975209a051648332d179cf0e42bf2
Polina Korkina <pvkorkina@gmail.com>
2023-03-09 19:21:20 +0500

    added 5-module 3-task (edit 2)

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,28 +1,27 @@
 function initCarousel() {
-  const width = document.querySelector('[data-carousel-holder]').offsetWidth;
+  const inner = document.querySelector('.carousel__inner');
   const slidesCount = document.querySelectorAll('.carousel__slide').length;
   const arrowLeft = document.querySelector('.carousel__arrow_left');
   const arrowRight = document.querySelector('.carousel__arrow_right');
-  let position = 0;
+  let current = 0;
+  let width;
 
   arrowLeft.style.display = 'none';
 
   arrowLeft.onclick = function() {
     arrowRight.style.display = '';
-    position += width;
-    translate(position);
-    if (position >= 0) this.style.display = 'none';
+    translate(--current);
+    if (current <= 0) this.style.display = 'none';
   }
   
   arrowRight.onclick = function() {
     arrowLeft.style.display = '';
-    position -= width;
-    translate(position);
-    if ( position <= -1 * width * (slidesCount - 1) ) this.style.display = 'none';
+    translate(++current);
+    if (current >= slidesCount - 1) this.style.display = 'none';
   }
 
-  function translate(position) {
-    document.querySelector('.carousel__inner').style
-      .transform = `translateX(${position}px)`;
+  function translate(current) {
+    width = document.querySelector('.carousel__slide').offsetWidth;
+    inner.style.transform = `translateX(${current * width * -1}px)`;
   }
 }
