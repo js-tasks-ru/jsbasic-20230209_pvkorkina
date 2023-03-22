9e0d5d3e0d13817f5c61c11f732cf11f69cba496
Polina Korkina <pvkorkina@gmail.com>
2023-03-22 21:40:57 +0500

    8-module 2-task

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,37 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner"></div>
+      </div>
+    `);
+
+    this.updateFilter();
+  }
+
+  updateFilter(filters = {}) {
+    Object.assign(this.filters, filters);
+
+    this.sub('inner').innerHTML = '';
+
+    for (let product of this.products) {
+      if (this.filters.noNuts && product.nuts) continue;
+      if (this.filters.vegeterianOnly && !product.vegeterian) continue;
+      if (this.filters.maxSpiciness < product.spiciness) continue;
+      if (
+        this.filters.category && this.filters.category !== product.category
+      ) continue;
+      
+      this.sub('inner').append(new ProductCard(product).elem);
+    }
+  }
+
+  sub(ref) {
+    return this.elem.querySelector(`.products-grid__${ref}`);
   }
 }
